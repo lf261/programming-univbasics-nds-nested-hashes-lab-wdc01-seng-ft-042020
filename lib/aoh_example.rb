b35c3a5e7b34c19fd905082e733590e62e721394
Luis Fernandez <luis.fernandez2093@gmail.com>
2020-04-14 03:31:28 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -24,11 +24,11 @@ def literal_aoh
   # held in the constants
   
   new_arr = [
-    DON_G = { name:  "Don Gately", occupation: "Live-in Staff" }
-JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" }
-PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
-KATE_G = { name:  "Kate Gompert", occupation: "None" }
-BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
+{ name:  "Don Gately", occupation: "Live-in Staff" },
+ { name:  "Joelle van Dyne", occupation: "Radio Personality" },
+ { name:  "Pat Monteseian", occupation: "Staff" },
+ { name:  "Kate Gompert", occupation: "None" },
+ { name:  "Bruce Green", occupation: "Fan of Mildred" },
     ]
 end
 
